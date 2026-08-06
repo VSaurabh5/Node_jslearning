3d4174c7460edbf11c35e5886c05337982d85e2f
Saurabh_Verma <sv2705052@gmail.com>
2026-08-06 20:45:38 +0530

    Learning embeded js for dynamic ui

--- testExpress/practiceExpress.js
+++ testExpress/practiceExpress.js
@@ -5,6 +5,8 @@ const homerouter=require('./routes/homerouter');
 const contactrouter=require('./routes/contactrouter');  
 
 const app=express();
+app.set('view engine','ejs');
+app.set('views','views');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded());
 app.use(homerouter);
--- testExpress/routes/homerouter.js
+++ testExpress/routes/homerouter.js
@@ -3,12 +3,14 @@ const express=require('express');
 const homerouter=express.Router();
 const path=require('path');
 const rootdir=require('./utils/pathutil');
-
+const registeredhome=[];
 homerouter.get('/',(req,res,next)=>{
     console.log('Handling / for GET',req.url,req.method);
+    registeredhome.push({housename: req.body.housename,});
     res.sendFile(path.join(rootdir,'views','home.html'));
 });
 
 
 
 module.exports=homerouter;
+exports.registeredhome=registeredhome;
